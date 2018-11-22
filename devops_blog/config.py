67081de1a02dbedcd73f97901193ebc6d0fcf878
root <devops@yikaobang.com.cn>
2018-11-22 09:22:12 +0800

    全局修改修改

--- devops_blog/config.py
+++ devops_blog/config.py
@@ -7,8 +7,66 @@
 		1. app加载的时候使用 app.config.from_object('configmodule.ProductionConfig')
 	  	2. Config 对象中必须的变量名称必须大写开头
 """
+from logging.config import dictConfig
+import os
+import time
+import logging
+ 
+basedir = os.path.abspath(os.path.dirname(__file__))
+log_path = basedir+os.sep+'logs'+ os.sep
+if not os.path.exists(log_path):
+    os.makedirs(log_path)
+
+# 日志格式配置
+dictConfig({
+        'version': 1,
+            'formatters': {
+                'default': {
+                            'format': '%(asctime)s [%(threadName)s:%(thread)d in %(module)s:] [task_id:%(name)s] [%(name)s:%(lineno)d] [%(levelname)s]- %(message)s'
+                 },
+                'file_access': {
+                            'format': '%(asctime)s [%(threadName)s:%(thread)d in %(module)s:] [task_id:%(name)s] [%(name)s:%(lineno)d] [%(levelname)s]- %(message)s'
+                 },
+                'file_error': {
+                            'format': '%(asctime)s [%(threadName)s:%(thread)d in %(module)s:] [task_id:%(name)s] [%(name)s:%(lineno)d] [%(levelname)s]- %(message)s'
+                 },
+            },
+            'handlers': {
+                        'wsgi': {
+                            'class': 'logging.StreamHandler',
+                            'stream': 'ext://flask.logging.wsgi_errors_stream',
+                            'formatter': 'default'
+                            },
+                        'file_access_handler':{
+                            'class': 'logging.handlers.RotatingFileHandler', # 自动切割
+                            'maxBytes': 1024 * 1024 * 20, # 20m 一个日志文件
+                            'filename': os.path.join(log_path, time.strftime('%Y_%m_%d',time.localtime())+"_access.log"),  # 日志文件
+                            'backupCount': 50, # 最多备份几个
+                            'level': 'DEBUG',
+                            'formatter': 'file_access', 
+                            'encoding': 'utf8'
+                            },
+                        'file_error_handler':{
+                            'class': 'logging.handlers.RotatingFileHandler', # 自动切割
+                            'maxBytes': 1024 * 1024 * 20, # 20m 一个日志文件
+                            'filename': os.path.join(log_path, time.strftime('%Y_%m_%d',time.localtime())+"_error.log"),  # 日志文件
+                            'backupCount': 50, # 最多备份几个
+                            'level': 'ERROR',
+                            'formatter': 'file_error', 
+                            'encoding': 'utf8'
+                        }
+            },
+            'root': {
+                    'level': 'DEBUG',
+                    'handlers': ['wsgi','file_access_handler','file_error_handler']
+            }
+})
+
 class Config:
-    pass
+    SQLALCHEMY_RECORD_QUERIES = True
+    SQLALCHEMY_NATIVE_UNICODE = 'utf8'  
+    TABLE_PREFIX = 'fb_'
+
 
 # 生产环境配置
 class ProductionConfig(Config):
@@ -38,6 +96,7 @@ class ProductionConfig(Config):
     AA = '2323'
     SECRET_KEY = b'233#_2L"3dQ8zc-\xcc]/'
 
+
 # 开发环境配置
 class DevelopmentConfig(Config):
     ENV = 'development'
@@ -50,6 +109,7 @@ class DevelopmentConfig(Config):
     MONGOALCHEMY_DATABASE = 'flask_blog'
     # mysql 连接
     SQLALCHEMY_DATABASE_URI="mysql+pymysql://root:123456@127.0.0.1:3306/flask_blog"
+    SQLALCHEMY_ECHO = True
     SQLALCHEMY_TRACK_MODIFICATIONS = True
     # 域名配置 正式环境必须配置这个参数
     #SERVER_NAME = 'blog.devops89.cn'
