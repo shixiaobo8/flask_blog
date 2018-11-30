e74605a7bfb7edbfa8d1e47cc956d0d56b1ce7a6
root <shixiaobo8@163.com>
2018-12-01 00:04:22 +0800

    api marshal

--- devops_blog/backend/api_resources.py
+++ devops_blog/backend/api_resources.py
@@ -3,14 +3,12 @@
 """
 	restful api route 类和类注册文件
 """
-from flask_restful import Resource
-from flask_restful import reqparse
+from flask_restful import abort,Resource,reqparse,fields,marshal_with,marshal
 from ..blog.models import Nav,subNav
 from .. import mysql_db
 from ..utils import generate_response,CustomFlaskErr
 from flask import request,current_app
-from flask_restful import abort
-
+import json
 
 # 测试api 类
 class ApiTest(Resource):
@@ -87,27 +85,51 @@ class NavApi(Resource):
         pass
 
 
+# 输出字段
+subNav_fields = {
+    'subtitle':fields.String(attribute='title'),
+    'suburl': fields.String(attribute='nav_url'),
+}
+
+Nav_fields = {
+    'id':fields.Integer,
+    'title':fields.String(attribute='navTitle'),
+    'type':fields.Integer(attribute='navType'),
+    'url': fields.String(attribute='navUrl'),
+    'subnavs': fields.List(fields.Nested(subNav_fields,allow_null=True,default=''),default='')
+}
+
 # 导航栏nav 处理列表类
 class NavListApi(Resource):
+    
     # 请求参数处理
     def __init__(self):
         self.reqparse = reqparse.RequestParser()
-        self.reqparse.add_argument('data', type = str, required = True, location='json')
-        #self.reqparse.add_argument('action', type = str, required = True, location='json')
+        self.reqparse.add_argument('page', type = int, default=1, location='args',help='第几个分页')
+        self.reqparse.add_argument('limit', type = int, default=10 , location='args',help='每页显示多少')
         super(NavListApi,self).__init__()
 
     # 获取(查询)nav 列表
+    #@marshal_with(Nav_fields,envelope='Nav')
     def get(self):
-        pass
+        args = self.reqparse.parse_args()
+        page_index = args['page']
+        page_size = args['limit']
+        # 先获取所有满足条件的nav isouter=True 表示left join all 方法得到一个列表,这里不使用paginate,会报错
+        navall = mysql_db.session.query(Nav).join(subNav,Nav.id==subNav.nav_Id,isouter=True).all()
+        # 分页
+        page_navs = navall[(page_index-1)*page_size:page_size*page_index]
+        return {'code':0,'count':len(navall),'cureent_page':page_index,"page_size":page_size,'data':marshal(page_navs,Nav_fields)}
 
     # 修改多个nav列表
     def post(self):
-        pass
+        return 'post test'
 
     # 增加多个列表
     def put(self):
         args = self.reqparse.parse_args()
         return {"data":args}
+    
     # 删除多个列表
     def delete(self):
         pass
